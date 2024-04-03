38b3f0510b09934fdda9606697d84af63a7cf067
Deepaks1827 <deepak81unnao@gmail.com>
2024-04-03 07:33:14 +0530

    variablr

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 console.log("Deepak shukla");
+
+//Basic Variables
+const accountId = 123;                   //block scope , cant changed.
+let accountEmail="deepak@gmail.com";      // block scope , can be changed.
+var accountNumber = 23456;                   //global scope can be changed.
+accountName ="Deepak";                           // no data type defined auto taken.
+
+console.table([accountId,accountEmail,accountNumber,accountName]);
