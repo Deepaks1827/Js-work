beb73dbb597c27191758c07fa279cd013335453b
Deepaks1827 <deepak81unnao@gmail.com>
2024-04-04 15:54:27 +0530

    Second Commit

--- test.js
+++ test.js
@@ -7,3 +7,57 @@ var accountNumber = 23456;                   //global scope can be changed.
 accountName ="Deepak";                           // no data type defined auto taken.
 
 console.table([accountId,accountEmail,accountNumber,accountName]);
+
+//Basic Datatypes
+
+//number
+//string
+//bigint
+//boolean  
+//null         => object but an empty value
+//undefined    => undefined  later can be defined.
+//symbol        
+
+//typeof tells the type of data.
+console.log(typeof null);
+
+//conversion in js
+
+let score = 33;
+console.log( score , typeof score);
+let score1 = "33"   
+let score3 = Number(score1);     // here we have done the conversion 
+console.log(score3, typeof score3);
+console.log(score1, typeof score1);
+
+let score2 = "33a"
+console.log(score2, typeof score2);
+let score4 = Number(score2); 
+console.log(score4, typeof score4);    //here it gives a not a number(NaN);
+
+let score5 = true;
+console.log(score5 , typeof score5);
+// true => 1   
+//false => 0
+//"" => false
+//"deepak" =>true 
+
+
+//Operations.
+console.log(3 + "a");
+console.log("a" +3);
+console.log("a " + 1 + 3);
+console.log(5 + 8 +"a " + 1 + 7);
+console.log(+true);
+console.log(+"");
+
+//Comparison'
+console.log("2">1);  //Here js converts string to the number
+console.log(null > 0); //> and == works differently that comparison converts null to number
+console.log(null == 0);
+console.log(null >= 0);
+
+//=== Strict comparison
+console.log("2" === 2);
+                                     
+
---
+++ test1.js
@@ -0,0 +1,47 @@
+//Data types
+//1 => Primitive --- String, Boolean, bigInt, Integer,null , undefined, Symbol.
+let id = Symbol("Deepak");
+let id1 = Symbol("Deepak");
+console.log(id===id1);
+
+let userEmail;   // here undefined value will be stored.
+const a1 = null;
+
+//2 => Non Primitive(Reference type) 
+//Array , Objects , Functions.
+let arr1 = [3,5,7];
+let onj1={
+    name:"Deepakji",
+    age:25
+}
+const func =()=>{
+                              //Arrow function
+}
+const func1 =function (){
+ console.log("Hello World");
+}
+
+func1();
+
+
+//*************************** */
+//Stack =>(Primitive)       Here we get only the copy of the memory 
+//Heap =>(Non Primitive).    Here we directly receive the reference of the memory means we directly change the value. 
+
+
+let myName = "Deepak";
+let myName1 = myName;
+myName1= "Jagdeesh";
+
+console.log(myName);
+console.log(myName1);
+
+let obj1 ={
+    name:"Deepak",
+    age:26
+}
+
+let obj2 = obj1
+obj2.name = "Shukla";
+console.log(obj1.name);
+console.log(obj2.name);
